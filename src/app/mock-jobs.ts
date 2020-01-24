d5ce4f9d80b45761dd08e25aa621a97bc34fc6b4
Jesse Richardson <jesse.richardson.career@gmail.com>
2020-01-24 14:13:20 -0600

    Configuration, setup and simple test of using SASS. Also installed Bootstrap via npm

--- src/app/mock-jobs.ts
+++ src/app/mock-jobs.ts
@@ -4,6 +4,4 @@ export const JOBS: Job[] = [
   { id: 11, companyName: "Saint Louis Chess Club", jobTitle: "Web Developer" },
   { id: 12, companyName: "Melco Embroidery", jobTitle: "Software Developer" },
   { id: 13, companyName: "Amadeus Consulting", jobTitle: "Web Developer" }
-
-
 ];
